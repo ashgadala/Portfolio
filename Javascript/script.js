0c26dd5f014e6243044e5e6c3474e31902f523ee
Aakriti <aakriti.1012@gmail.com>
2017-06-07 15:59:49 +0530

    added jquery ui and final structure of code

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,40 +1,12 @@
 $(document).ready(function() {
 
   $('a').click(function() {
-
     $('a').removeClass('selected');
     $(this).addClass('selected');
-
     current = $(this);
-
     $('.wrapper').scrollTo($(this).attr('href'), 800);
-
     return false;
   });
 
-//   $(window).resize(function() {
-//     resizePanel();
-//   });
-
+  
 });
-
-// function resizePanel() {
-
-//   width = $(window).width();
-//   height = $(window).height();
-
-//   mask_width = width * $('.item').length;
-
-//   $('#debug').html(width + ' ' + height + ' ' + mask_width);
-
-//   $('#wrapper, .item').css({
-//     width: width,
-//     height: height
-//   });
-//   $('#mask').css({
-//     width: mask_width,
-//     height: height
-//   });
-//   $('#wrapper').scrollTo($('a.selected').attr('href'), 0);
-
-// }
